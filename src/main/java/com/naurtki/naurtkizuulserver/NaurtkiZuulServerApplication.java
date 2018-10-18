0a7173c36396b5a7992c497f67d88a797b066e58
Jiptesh Moharana <jiptesh.moharana@iddinkgroup.com>
2018-10-18 15:34:41 +0530

    zuul client - first commit

---
+++ src/main/java/com/naurtki/naurtkizuulserver/NaurtkiZuulServerApplication.java
@@ -0,0 +1,12 @@
+package com.naurtki.naurtkizuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaurtkiZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaurtkiZuulServerApplication.class, args);
+	}
+}
